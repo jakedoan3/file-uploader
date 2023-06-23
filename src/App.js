4076828d3cf511e7fa3d793b4dd018a78c467e38
Jake Doan <jakedoan3@gmail.com>
2023-06-23 08:07:13 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase
 import { Route, Routes} from 'react-router-dom'
 
 
-//last updated June 21, 2023
+//last updated June 22, 2023
 
 //TO-DO:
 
